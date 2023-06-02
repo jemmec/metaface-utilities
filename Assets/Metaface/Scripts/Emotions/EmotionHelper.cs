6238f721881057ad0617c380b4aee8038cfa99c1
jemmec <jeremy.mcdade@unisa.edu.au>
2023-06-02 12:58:53 +0930

    Started implementing EmotionHelper

---
+++ Assets/Metaface/Scripts/Emotions/EmotionHelper.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Movement.Effects;
+using UnityEngine;
+
+/// <summary>
+/// Based off Meta's Oculus.Movement.Effects.SmileEffect
+/// </summary>
+public class EmotionHelper : MonoBehaviour
+{
+        [SerializeField]
+        protected MacroFacialExpressionDetector _facialExpressionDetector;
+
+
+}
+
